531b8414c6a535ef2efb864990d72d0b67abbea3
Tamás Pap <43545373+belhart@users.noreply.github.com>
2020-08-16 14:15:29 +0200

    index started

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
